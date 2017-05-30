9d23905e8f20d037721e3a5feedf6982ae4dc8ac
Kevin Tam <kevin@glorat.net>
2017-05-30 12:19:34 +0800

    select-on-click for shuttle entry

--- client/app.js
+++ client/app.js
@@ -157,3 +157,17 @@ function GauntletCtrl($scope, $http, Combinatorics, Gauntlet) {
     });
   };
 }
+
+angular.module('myApp').directive('selectOnClick', ['$window', function ($window) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.on('click', function () {
+        if (!$window.getSelection().toString()) {
+          // Required for mobile Safari
+          this.setSelectionRange(0, this.value.length)
+        }
+      });
+    }
+  };
+}]);
