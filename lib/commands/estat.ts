5d921a6d70b69ac3ee975ce4a3f2ac86d591097e
Kevin Tam <kevin@glorat.net>
2025-11-16 12:19:30 +0800

    feat: switch to datacore for crew info

--- lib/commands/estat.ts
+++ lib/commands/estat.ts
@@ -1,49 +1,50 @@
-import {SlashCommandBuilder} from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
 
-const Clapp = require('../modules/clapp-discord');
-import * as _ from 'underscore';
-import chars = require('../chars');
+const Clapp = require("../modules/clapp-discord");
+import * as _ from "underscore";
+import chars = require("../chars");
 //import cfg from '../../config';
-import * as API from '../Interfaces';
-import {argOrFlagToBuilder} from "../cli";
+import * as API from "../Interfaces";
+import { argOrFlagToBuilder } from "../cli";
 
-const wikiurl = 'sttwiki.org';
+const DATACORE_CREW_URL = "https://datacore.app/crew";
 
 const flags = [
   {
-    name: 'names',
-    desc: 'search string',
-    type: 'string',
-    default: '',
-    required: true // only respected for slash commands
+    name: "names",
+    desc: "search string",
+    type: "string",
+    default: "",
+    required: true, // only respected for slash commands
   },
   {
-    name: 'stars',
-    desc: 'Number of fused stars to query at',
-    alias: 's',
-    type: 'number',
-    default: 0
+    name: "stars",
+    desc: "Number of fused stars to query at",
+    alias: "s",
+    type: "number",
+    default: 0,
   },
   {
-    name: 'level',
-    desc: 'Skill level to query at. Should be 1,10,20,30,40,50,60,70,80,90,100',
-    alias: 'l',
-    type: 'number',
-    default: 100
-  }
-]
+    name: "level",
+    desc: "Skill level to query at. Should be 1,10,20,30,40,50,60,70,80,90,100",
+    alias: "l",
+    type: "number",
+    default: 100,
+  },
+];
 
 module.exports = new Clapp.Command({
   name: "estats",
   desc: "query extended stats for characters",
 
-// Command function
-  fn: (argv:any, context:API.Context) => new Promise((fulfill, reject) => {
-    const args = argv.args;
-    //Emojis are broken in android so we manually do it
-    // const emojify = context.emojify;
+  // Command function
+  fn: (argv: any, context: API.Context) =>
+    new Promise((fulfill, reject) => {
+      const args = argv.args;
+      //Emojis are broken in android so we manually do it
+      // const emojify = context.emojify;
 
-/*
+      /*
 white medium star
     Unicode: U+2B50 U+FE0F, UTF-8: E2 AD 90 EF B8 8F
 🌑
@@ -51,179 +52,220 @@ new moon
     Unicode: U+1F311, UTF-8: F0 9F 8C 91
 */
 
-    const emojify : API.EmojiFn = function(x) {
-      const custom : any = {'1star':'\u2B50', '1darkstar' :String.fromCodePoint(0x1F311)};
-      if (custom[x]) {
-        return custom[x];
-      }
-      else {
-        return x.toUpperCase();
-      }
-    };
-
-    function handleName(name:string, starsArg:number, level:number, ssr:any) {
-      chars.wikiLookup(name, function(err:any,info:chars.CharInfo) {
-        //chars.ssrLookup(name, function(err,info) {
-        if (err) {
-          fulfill(err);
+      const emojify: API.EmojiFn = function (x) {
+        const custom: any = {
+          "1star": "\u2B50",
+          "1darkstar": String.fromCodePoint(0x1f311),
+        };
+        if (custom[x]) {
+          return custom[x];
+        } else {
+          return x.toUpperCase();
         }
-        else {
+      };
 
-          const stars = info.stars;
-          const skill = info.skill;
-          const char = info.char;
-          const wiki = info.wiki.replace('/wiki/','');
+      function handleName(
+        name: string,
+        starsArg: number,
+        level: number,
+        ssr: any
+      ) {
+        chars.wikiLookup(name, function (err: any, info: chars.CharInfo) {
+          //chars.ssrLookup(name, function(err,info) {
+          if (err) {
+            fulfill(err);
+          } else {
+            const stars = info.stars;
+            const skill = info.skill;
+            const char = info.char;
 
-          let starStat = function(s:number) {
-            const starStr = _.range(s).map(x => emojify('1star')).join('');
-            const darkStr = _.range(stars - s).map(x => emojify('1darkstar')).join('');
-            const starSk = _.filter(skill, sk => sk.stars === s && sk.level === level);
-            const skStr = _.map(starSk, sk => `${emojify(sk.skill)} ${sk.base} (${sk.min}-${sk.max})`).join(' ');
-            return `${starStr}${darkStr} - ${skStr}`;
-          };
+            let starStat = function (s: number) {
+              const starStr = _.range(s)
+                .map((x) => emojify("1star"))
+                .join("");
+              const darkStr = _.range(stars - s)
+                .map((x) => emojify("1darkstar"))
+                .join("");
+              const starSk = _.filter(
+                skill,
+                (sk) => sk.stars === s && sk.level === level
+              );
+              const skStr = _.map(
+                starSk,
+                (sk) => `${emojify(sk.skill)} ${sk.base} (${sk.min}-${sk.max})`
+              ).join(" ");
+              return `${starStr}${darkStr} - ${skStr}`;
+            };
 
-          //const levelStr = (level!==100) ? `Level ${level}: ` : '';
-          //let header = `(${char}) - ${levelStr}${info.traits}`;
+            //const levelStr = (level!==100) ? `Level ${level}: ` : '';
+            //let header = `(${char}) - ${levelStr}${info.traits}`;
 
-          // Char stats
-          let msg = '';
-          if (starsArg > 0 && starsArg < stars) {msg += starStat(starsArg) + '\n';}
-          else if (stars === 5) { msg += starStat(1) + '\n';}
-          msg += starStat(stars);
+            // Char stats
+            let msg = "";
+            if (starsArg > 0 && starsArg < stars) {
+              msg += starStat(starsArg) + "\n";
+            } else if (stars === 5) {
+              msg += starStat(1) + "\n";
+            }
+            msg += starStat(stars);
 
-          // Char rankings
-          // Override filter to do exact match
-          let allChars = chars.allCrewEntries().filter(x=>x.stars === stars);
-          let baseBest = chars.bestChars(allChars, info.stars,
-            (starsArg > 0 && starsArg < stars) ? starsArg : info.stars, 'base', 100, '', '');
-          let baseRank = _.findIndex(baseBest, x=>x.name === info.name) + 1;
-          let gauntletBest = chars.bestChars(allChars, info.stars,
-            (starsArg > 0 && starsArg < stars) ? starsArg : info.stars, 'gauntlet', 100, '', '');
-          let gauntletRank = _.findIndex(gauntletBest, x=>x.name === info.name) + 1;
-          let voyageBest = chars.bestChars(allChars, info.stars,
-            (starsArg > 0 && starsArg < stars) ? starsArg : info.stars, 'avg', 100, '', '');
-          let voyageRank = _.findIndex(voyageBest, x=>x.name === info.name) + 1;
+            // Char rankings
+            // Override filter to do exact match
+            let allChars = chars
+              .allCrewEntries()
+              .filter((x) => x.stars === stars);
+            let baseBest = chars.bestChars(
+              allChars,
+              info.stars,
+              starsArg > 0 && starsArg < stars ? starsArg : info.stars,
+              "base",
+              100,
+              "",
+              ""
+            );
+            let baseRank =
+              _.findIndex(baseBest, (x) => x.name === info.name) + 1;
+            let gauntletBest = chars.bestChars(
+              allChars,
+              info.stars,
+              starsArg > 0 && starsArg < stars ? starsArg : info.stars,
+              "gauntlet",
+              100,
+              "",
+              ""
+            );
+            let gauntletRank =
+              _.findIndex(gauntletBest, (x) => x.name === info.name) + 1;
+            let voyageBest = chars.bestChars(
+              allChars,
+              info.stars,
+              starsArg > 0 && starsArg < stars ? starsArg : info.stars,
+              "avg",
+              100,
+              "",
+              ""
+            );
+            let voyageRank =
+              _.findIndex(voyageBest, (x) => x.name === info.name) + 1;
 
-          // Useful links
-          let embed = {
-            color: 3447003,
-            /*author: {
+            // Useful links
+            const symbol = info.symbol;
+            const detailUrl = symbol
+              ? `${DATACORE_CREW_URL}/${symbol}`
+              : undefined;
+
+            let embed = {
+              color: 3447003,
+              /*author: {
               name: name,
               icon_url: 'https://${wikiurl}/w/images/thumb/a/ae/Captain_Kirk_Head.png/180px-Captain_Kirk_Head.png'
             },*/
-            title: name,
-            url: `https://${wikiurl}/wiki/${wiki}`,
-            //description: 'Description.',
-            fields: [
-              {
-                name: 'Traits',
-                value: info.traits,
-                inline:true
-              },
-              {
-                name: 'More Traits',
-                value: info.traits_hidden.join(', '),
-                inline:true
-              },
-              {
-                name: 'Stats',
-                value: msg
-              },
-              {
-                name: 'Base Rank',
-                value: `${baseRank} of ${baseBest.length} (${stars} stars)`,
-                inline: true
-              },
-              {
-                name: 'Gauntlet Rank',
-                value: `${gauntletRank} of ${gauntletBest.length} (${stars} stars)`,
-                inline: true
-              },
-              {
-                name: 'Voyage Rank',
-                value: `${voyageRank} of ${voyageBest.length} (${stars} stars)`,
-                inline: true
-              },
-              {
-                name: 'Character',
-                value: char,
-                inline:true
-              },
-              {
-                name: 'Avg Chrons',
-                value: ssr.avgChrons + '',
-                inline:true
-              },
-              {
-                name: 'Difficulty',
-                value: chars.generateDifficulty(ssr),
-                inline:true
-              }
-            ],
-            thumbnail:{
-              url:`https://${wikiurl}` + info.headImage
-            }
-            //timestamp: new Date(),
-          };
+              title: name,
+              url: detailUrl,
+              //description: 'Description.',
+              fields: [
+                {
+                  name: "Traits",
+                  value: info.traits,
+                  inline: true,
+                },
+                {
+                  name: "More Traits",
+                  value: info.traits_hidden.join(", "),
+                  inline: true,
+                },
+                {
+                  name: "Stats",
+                  value: msg,
+                },
+                {
+                  name: "Base Rank",
+                  value: `${baseRank} of ${baseBest.length} (${stars} stars)`,
+                  inline: true,
+                },
+                {
+                  name: "Gauntlet Rank",
+                  value: `${gauntletRank} of ${gauntletBest.length} (${stars} stars)`,
+                  inline: true,
+                },
+                {
+                  name: "Voyage Rank",
+                  value: `${voyageRank} of ${voyageBest.length} (${stars} stars)`,
+                  inline: true,
+                },
+                {
+                  name: "Character",
+                  value: char,
+                  inline: true,
+                },
+                {
+                  name: "Avg Chrons",
+                  value: ssr.avgChrons + "",
+                  inline: true,
+                },
+                {
+                  name: "Difficulty",
+                  value: chars.generateDifficulty(ssr),
+                  inline: true,
+                },
+              ],
+              thumbnail: info.headImage ? { url: info.headImage } : undefined,
+              //timestamp: new Date(),
+            };
 
-          context.embed = embed;
-          fulfill('EMBED');
-        }
-      });
-
-    }
-
-    const matchArgs = argv.flags.names ? argv.flags.names.split(' ') : [args.name1, args.name2, args.name3]
-
-    chars.matchOne(function(err,name) {
-      if (err) {
-        fulfill(err);
-      }
-      else {
-        let nm = <string>name;
-        chars.ssrLookup(nm, (ssr:any) => {
-          handleName(nm, argv.flags.stars, argv.flags.level, ssr);
+            context.embed = embed;
+            fulfill("EMBED");
+          }
         });
-
       }
-    }, ...matchArgs);
-  }),
+
+      const matchArgs = argv.flags.names
+        ? argv.flags.names.split(" ")
+        : [args.name1, args.name2, args.name3];
+
+      chars.matchOne(function (err, name) {
+        if (err) {
+          fulfill(err);
+        } else {
+          let nm = <string>name;
+          chars.ssrLookup(nm, (ssr: any) => {
+            handleName(nm, argv.flags.stars, argv.flags.level, ssr);
+          });
+        }
+      }, ...matchArgs);
+    }),
   args: [
     {
-      name: 'name1',
-      desc: 'Name of character',
-      type: 'string',
-      default: '',
-      required: false
+      name: "name1",
+      desc: "Name of character",
+      type: "string",
+      default: "",
+      required: false,
     },
     {
-      name: 'name2',
-      desc: 'Name of character',
-      type: 'string',
-      default: '',
-      required: false
+      name: "name2",
+      desc: "Name of character",
+      type: "string",
+      default: "",
+      required: false,
     },
     {
-      name: 'name3',
-      desc: 'Name of character',
-      type: 'string',
-      default: '',
-      required: false
-    }
-
+      name: "name3",
+      desc: "Name of character",
+      type: "string",
+      default: "",
+      required: false,
+    },
   ],
   flags,
   opts: {
     slashCommandBuilder: () => {
-      const b = new SlashCommandBuilder()
-      b.setName('estats').setDescription('query extended stats for characters')
-      flags.forEach(flag => {
-        argOrFlagToBuilder(b, {required: false, ...flag})
-      })
-      return b
-    }
-  }
-
+      const b = new SlashCommandBuilder();
+      b.setName("estats").setDescription("query extended stats for characters");
+      flags.forEach((flag) => {
+        argOrFlagToBuilder(b, { required: false, ...flag });
+      });
+      return b;
+    },
+  },
 });
-
-
--- lib/datacore.ts
+++ lib/datacore.ts
@@ -1,101 +1,152 @@
 import CONFIG from "./dcmodel/CONFIG";
 import { CrewMember } from "./dcmodel/crew";
-import { CrewAvatar } from "./wikiparse";
+import * as fs from "async-file";
+import fetch from "node-fetch";
 
 export const STATIC_URL = `https://datacore.app/structured/`;
 export const DATACORE_ASSETS_URL = `https://assets.datacore.app/`;
 
-export async function downloadDataCoreCrew() {
+// Download raw Datacore crew.json as typed CrewMember[]
+export async function downloadDataCoreCrew(): Promise<CrewMember[] | null> {
   const crewUrl = `${STATIC_URL}crew.json`;
-  let result = await fetch(crewUrl);
+  const result = await fetch(crewUrl);
 
-  if (result.ok) {
-    const crew = (await result.json()) as CrewMember[];
-    return crew.map((c) => ({
-        ...c,
-        portrait: { file: `${DATACORE_ASSETS_URL}${c.imageUrlPortrait}` },
-        full_body: { file: `${DATACORE_ASSETS_URL}${c.imageUrlFullBody}` },
-        default_avatar: false,
-        hide_from_cryo: false,
-        skills: c.skill_order,
-        icon: { file: `${DATACORE_ASSETS_URL}${c.imageUrlPortrait}` },
-        wiki: ''
-    } as CrewAvatar))
+  if (!result.ok) {
+    return null;
   }
-  return null;
+
+  const crew = (await result.json()) as CrewMember[];
+  return crew;
 }
 
-export function getShortNameFromTrait(trait: string, crewGroup: CrewMember[] | CrewMember, preferEnglish = true) {
-	switch(trait) {
-		case "dax":
-			let daxname = '';
-			if (Array.isArray(crewGroup)) {
-				daxname = (preferEnglish ? crewGroup[0].short_name_english : '') || crewGroup[0].short_name;
-			}
-			else {
-				daxname = (preferEnglish ? crewGroup.short_name_english : '') || crewGroup.short_name;
-			}
-			if (daxname === 'Ezri') return daxname;
-			return 'Dax';
-		case "tpring":
-			return "T'Pring";
-		case "mbenga":
-			return "M'Benga";
-		case "gburnham":
-			return "G. Burnham";
-		case "burnham":
-			return "M. Burnham";
-		case "mburnham_sr":
-			return "M. Burnham, Sr."
+export function getShortNameFromTrait(
+  trait: string,
+  crewGroup: CrewMember[] | CrewMember,
+  preferEnglish = true
+) {
+  switch (trait) {
+    case "dax":
+      let daxname = "";
+      if (Array.isArray(crewGroup)) {
+        daxname =
+          (preferEnglish ? crewGroup[0].short_name_english : "") ||
+          crewGroup[0].short_name;
+      } else {
+        daxname =
+          (preferEnglish ? crewGroup.short_name_english : "") ||
+          crewGroup.short_name;
+      }
+      if (daxname === "Ezri") return daxname;
+      return "Dax";
+    case "tpring":
+      return "T'Pring";
+    case "mbenga":
+      return "M'Benga";
+    case "gburnham":
+      return "G. Burnham";
+    case "burnham":
+      return "M. Burnham";
+    case "mburnham_sr":
+      return "M. Burnham, Sr.";
 
-		default:
-			if (Array.isArray(crewGroup)) {
-				return (preferEnglish ? crewGroup[0].short_name_english : '') || crewGroup[0].short_name;
-			}
-			else {
-				return (preferEnglish ? crewGroup.short_name_english : '') || crewGroup.short_name;
-			}
-	}
+    default:
+      if (Array.isArray(crewGroup)) {
+        return (
+          (preferEnglish ? crewGroup[0].short_name_english : "") ||
+          crewGroup[0].short_name
+        );
+      } else {
+        return (
+          (preferEnglish ? crewGroup.short_name_english : "") ||
+          crewGroup.short_name
+        );
+      }
+  }
 }
 
-export const crewVariantIgnore = ['sam_lavelle_crew', 'jack_crusher_crew'];
+export const crewVariantIgnore = ["sam_lavelle_crew", "jack_crusher_crew"];
 
 export function getVariantTraits(subject: CrewMember | string[]): string[] {
-	const ignore = [
-		'female', 'male',
-		'artificial_life', 'nonhuman', 'organic', 'species_8472',
-		'admiral', 'captain', 'commander', 'lieutenant_commander', 'lieutenant', 'ensign', 'general', 'nagus', 'first_officer',
-		'ageofsail', 'bridge_crew', 'evsuit', 'gauntlet_jackpot', 'mirror', 'niners', 'crewman',
-		'crew_max_rarity_5', 'crew_max_rarity_4', 'crew_max_rarity_3', 'crew_max_rarity_2', 'crew_max_rarity_1'
-	];
-	const ignoreRe = [
-		/^exclusive_/,		/* exclusive_ crew, e.g. bridge, collection, fusion, gauntlet, honorhall, voyage */
-		/^[a-z]{3}\d{4}$/,	/* mega crew, e.g. feb2023 and apr2023 */
-		/^[a-z]{4}\d{4}$/	/* mega crew, e.g. june2024 and july2024 */
-	];
-	const variantTraits = [] as string[];
+  const ignore = [
+    "female",
+    "male",
+    "artificial_life",
+    "nonhuman",
+    "organic",
+    "species_8472",
+    "admiral",
+    "captain",
+    "commander",
+    "lieutenant_commander",
+    "lieutenant",
+    "ensign",
+    "general",
+    "nagus",
+    "first_officer",
+    "ageofsail",
+    "bridge_crew",
+    "evsuit",
+    "gauntlet_jackpot",
+    "mirror",
+    "niners",
+    "crewman",
+    "crew_max_rarity_5",
+    "crew_max_rarity_4",
+    "crew_max_rarity_3",
+    "crew_max_rarity_2",
+    "crew_max_rarity_1",
+  ];
+  const ignoreRe = [
+    /^exclusive_/ /* exclusive_ crew, e.g. bridge, collection, fusion, gauntlet, honorhall, voyage */,
+    /^[a-z]{3}\d{4}$/ /* mega crew, e.g. feb2023 and apr2023 */,
+    /^[a-z]{4}\d{4}$/ /* mega crew, e.g. june2024 and july2024 */,
+  ];
+  const variantTraits = [] as string[];
 
-	if ("length" in subject) {
-		subject.forEach(trait => {
-			if (!CONFIG.SERIES.includes(trait) && !ignore.includes(trait) && !ignoreRe.reduce((prev, curr) => prev || curr.test(trait), false)) {
-				variantTraits.push(trait);
-			}
-		});
-	}
-	else {
-		subject.traits_hidden.forEach(trait => {
-			if (!CONFIG.SERIES.includes(trait) && !ignore.includes(trait) && !ignoreRe.reduce((prev, curr) => prev || curr.test(trait), false)) {
-				variantTraits.push(trait);
-			}
-		});
-	}
+  if ("length" in subject) {
+    subject.forEach((trait) => {
+      if (
+        !CONFIG.SERIES.includes(trait) &&
+        !ignore.includes(trait) &&
+        !ignoreRe.reduce((prev, curr) => prev || curr.test(trait), false)
+      ) {
+        variantTraits.push(trait);
+      }
+    });
+  } else {
+    subject.traits_hidden.forEach((trait) => {
+      if (
+        !CONFIG.SERIES.includes(trait) &&
+        !ignore.includes(trait) &&
+        !ignoreRe.reduce((prev, curr) => prev || curr.test(trait), false)
+      ) {
+        variantTraits.push(trait);
+      }
+    });
+  }
 
-	return variantTraits;
+  return variantTraits;
 }
 
-(async () => {
-  const results = await downloadDataCoreCrew();
-  if (results?.length) {
-    console.log(`${results.length} converted to GotBot format.`);
+async function main() {
+  try {
+    const rawCrew = await downloadDataCoreCrew();
+    if (!rawCrew || !rawCrew.length) {
+      console.log("No crew found from Datacore");
+      return;
+    }
+
+    await fs.writeFile("data/datacore-crew.json", JSON.stringify(rawCrew));
+    console.log(
+      `${rawCrew.length} Datacore crew written to data/datacore-crew.json`
+    );
+  } catch (e) {
+    console.error("Error downloading Datacore crew", e);
   }
-})();
+}
+
+if (require.main === module) {
+  // When executed via ts-node, perform download and caching into ./data
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  main();
+}
